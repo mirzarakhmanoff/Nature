0c10de22663c656ddb99645c8d67bf94a0a41479
Ulugbek <mirzarakhmanoff@gmail.com>
2024-08-02 00:30:47 +0500

    add

--- src/components/clean/Clean.jsx
+++ src/components/clean/Clean.jsx
@@ -8,29 +8,32 @@ const Clean = () => {
     <div className="container">
       {" "}
       <div className="clean">
-        <div className="clean__left">
-          <h2>Clean and fragrant soy wax</h2>
-          <p>Made for your home and for your wellness</p>
+        <div className="clean__items">
+          <div className="clean__left">
+            <h2>Clean and fragrant soy wax</h2>
+            <p>Made for your home and for your wellness</p>
 
-          <span>
-            <CiCircleCheck /> Eco-sustainable:All recyclable materials, 0% CO2
-            emissions
-          </span>
-          <span>
-            <CiCircleCheck /> Hyphoallergenic: 100% natural, human friendly
-            ingredients
-          </span>
-          <span>
-            <CiCircleCheck /> Handmade: All candles are craftly made with love.
-          </span>
-          <span>
-            <CiCircleCheck /> Long burning: No more waste. Created for last
-            long.
-          </span>
-          <a href="#">Learn More</a>
-        </div>
-        <div className="clean__right">
-          <img src={cleanImg} alt="" />
+            <span>
+              <CiCircleCheck /> Eco-sustainable:All recyclable materials, 0% CO2
+              emissions
+            </span>
+            <span>
+              <CiCircleCheck /> Hyphoallergenic: 100% natural, human friendly
+              ingredients
+            </span>
+            <span>
+              <CiCircleCheck /> Handmade: All candles are craftly made with
+              love.
+            </span>
+            <span>
+              <CiCircleCheck /> Long burning: No more waste. Created for last
+              long.
+            </span>
+            <a href="#">Learn More</a>
+          </div>
+          <div className="clean__right">
+            <img src={cleanImg} alt="" />
+          </div>
         </div>
       </div>
     </div>
